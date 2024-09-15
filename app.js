2fbc5ce2f7ea64163e059eaf87e9098a8ce3d162
DeveshMhaisne <devesh.mhaisne@gmail.com>
2024-09-15 17:18:01 +0530

    Fixed errors in new and edit.ejs

--- app.js
+++ app.js
@@ -39,10 +39,10 @@ app.get("/", (req,res) => {
 
 
 const validateListing = (req,res,next)=> {
-    let (error) = listingSchema.validate(req.body);
+    let {error} = listingSchema.validate(req.body);
     if(error){
         let errMsg = error.details.map((el)=> el.message).join(",");
-        throw new ExpressError(400, result.errMsg);
+        throw new ExpressError(400, errMsg);
     }else{
         next();
     }
