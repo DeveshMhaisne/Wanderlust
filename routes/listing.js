3b3532a8f7b7b53c71f50b2a6d82e5a1d9c10e43
DeveshMhaisne <devesh.mhaisne@gmail.com>
2024-09-18 13:44:50 +0530

    created review.js

--- routes/listing.js
+++ routes/listing.js
@@ -5,19 +5,15 @@ const ExpressError = require("../utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
 
-
-
-
 const validateListing = (req, res, next) => {
-    let { error } = listingSchema.validate(req.body);
-    if (error) {
-      let errMsg = error.details.map((el) => el.message).join(",");
-      throw new ExpressError(400, errMsg);
-    } else {
-      next();
-    }
-  };
-
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
 
 // Index Route
 router.get(
@@ -85,6 +81,5 @@ router.delete(
     res.redirect("/listings");
   })
 );
-
 
 module.exports = router;
