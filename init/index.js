a0e3092287fc382dd62f9905442442f0d7dd28b4
DeveshMhaisne <devesh.mhaisne@gmail.com>
2024-10-12 17:17:40 +0530

    modified some files

--- init/index.js
+++ init/index.js
@@ -2,25 +2,28 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-    .then(() => { 
-        console.log("connected to DB");
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async () => {
-    // await Listing.deleteMany({});
-    await Listing.insertMany(initData.data);
-    console.log("data was inititalised");
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "67092cd3df93954e6a8aabf9",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data was inititalised");
 };
 
 initDB();
--- routes/review.js
+++ routes/review.js
@@ -2,20 +2,9 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
-const { listingSchema, reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-
-
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
+const { validateReview } = require("../middleware.js");
 
 // Reviews
 // Post Review Route
@@ -30,7 +19,7 @@ router.post(
 
     await newReview.save();
     await listing.save();
-    req.flash("success","New Review Created!");
+    req.flash("success", "New Review Created!");
 
     res.redirect(`/listings/${listing._id}`);
   })
@@ -44,8 +33,7 @@ router.delete(
 
     await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
-    req.flash("success","Review Deleted!");
-
+    req.flash("success", "Review Deleted!");
 
     res.redirect(`/listings/${id}`);
   })
