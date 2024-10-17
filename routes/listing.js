c20e258c10b2a1b587ca208c95bc4de9fb129419
DeveshMhaisne <devesh.mhaisne@gmail.com>
2024-10-17 17:16:25 +0530

    created .env and cloudconfig.js

--- routes/listing.js
+++ routes/listing.js
@@ -4,20 +4,19 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
-const multer = require('multer')
-const upload = multer({dest : 'uploads/'})
+const multer = require("multer");
+const { storage } = require("../cloudconfig.js");
+const upload = multer({ storage }); 
 
 router
   .route("/")
   .get(wrapAsync(listingController.index))
-  // .post(
-  //   isLoggedIn,
-  //   validateListing,
-  //   wrapAsync(listingController.createListing)
-  // );
-  .post( upload.single('listing[image][url]'), (req,res) => {
-    res.send(req.file);
-  })
+  .post(
+    isLoggedIn,
+    upload.single("listing[image][url]"),
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
 
 // New Route
 router.get("/new", isLoggedIn, listingController.renderNewform);
@@ -28,6 +27,7 @@ router
   .put(
     isLoggedIn,
     isOwner,
+    upload.single("listing[image][url]"),
     validateListing,
     wrapAsync(listingController.updateListing)
   )
